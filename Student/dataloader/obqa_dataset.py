23f62568bfec3f61a8b5cef93cabeedbd60dc98f
PopoDev <popocompte@gmail.com>
2024-02-17 12:05:38 -0800

    Tokenizer padding to max_length

--- Student/dataloader/obqa_dataset.py
+++ Student/dataloader/obqa_dataset.py
@@ -28,8 +28,8 @@ class OBQADataset(Dataset):
 
         prompt = prompt_prefix + question
 
-        encoding = self.tokenizer([prompt]*len(choices), choices, return_tensors="pt", padding=True, truncation=True, max_length=self.max_length)
+        encoding = self.tokenizer([prompt]*len(choices), choices, return_tensors="pt", padding='max_length', truncation=True, max_length=self.max_length)
         encoding['labels'] = label
-        
+
         return encoding
     
