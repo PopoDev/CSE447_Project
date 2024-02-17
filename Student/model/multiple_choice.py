71f926adec6190e364b7d5119270681642872908
PopoDev <popocompte@gmail.com>
2024-02-17 12:51:34 -0800

    Roberta Model labels batch_size

--- Student/model/multiple_choice.py
+++ Student/model/multiple_choice.py
@@ -1,13 +1,13 @@
 import torch
-from transformers import AutoTokenizer, RobertaForMultipleChoice
+from transformers import RobertaForMultipleChoice
 
 class RobertaPromptForMultipleChoice(torch.nn.Module):
     def __init__(self):
         super().__init__()
-        self.tokenizer = AutoTokenizer.from_pretrained("roberta-base")
         self.model = RobertaForMultipleChoice.from_pretrained("roberta-base")
 
     def forward(self, input_ids, attention_mask, labels):
-        outputs = self.model(input_ids=input_ids, attention_mask=attention_mask, labels=labels)
+        batch_size = input_ids.shape[0]
+        outputs = self.model(input_ids=input_ids, attention_mask=attention_mask, labels=labels.view(batch_size, 1))
         return outputs
     
