91ef392ef0d4d10aa62715e8b6a85c35363b5d56
PopoDev <popocompte@gmail.com>
2024-03-09 17:19:26 -0800

    Add debug SentenceBERT

--- Student/run_sbert.py
+++ Student/run_sbert.py
@@ -1,12 +1,15 @@
 import json
+import argparse
 from tqdm import tqdm
 from dataloader.dataset import load_openbookqa_data
 from model.sentence_similarity import SentenceBERTModel
 
-def add_openbook_facts(data):
+def add_openbook_facts(data, debug=False):
     sbert = SentenceBERTModel(path='./data/openbook.txt')
 
-    for i, item in enumerate(tqdm(data)):
+    max_iters = 10 if debug else len(data)
+
+    for item in tqdm(data[:max_iters]):
         question = item['question_stem']
         choices = item['choices']['text']
         sentence = question + ": " + ", ".join(choices)
@@ -14,16 +17,20 @@ def add_openbook_facts(data):
         item['facts'] = facts
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--debug", default=False)
+    args = parser.parse_args()
+
     train_data, val_data, test_data = load_openbookqa_data()
 
-    with open('./data/train_facts.jsonl', 'w') as file:
-        add_openbook_facts(train_data)
+    with open('./data/train.facts.jsonl', 'w') as file:
+        add_openbook_facts(train_data, debug=args.debug)
         json.dump(train_data, file)
 
-    with open('./data/dev_facts.jsonl', 'w') as file:
-        add_openbook_facts(val_data)
+    with open('./data/dev.facts.jsonl', 'w') as file:
+        add_openbook_facts(val_data, debug=args.debug)
         json.dump(val_data, file)
 
-    with open('./data/test_facts.jsonl', 'w') as file:
-        add_openbook_facts(test_data)
+    with open('./data/test.facts.jsonl', 'w') as file:
+        add_openbook_facts(test_data, debug=args.debug)
         json.dump(test_data, file)
